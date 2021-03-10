bfc274233158bef5e4f1445ee88ab643ad6d67c9
Wassim Boukries <wassim.boukries@etu.uca.fr>
2021-03-10 19:59:13 +0100

    Scroll down auto & click() button

--- Front/ClientApp/src/components/FetchData.js
+++ Front/ClientApp/src/components/FetchData.js
@@ -10,7 +10,6 @@ export class FetchData extends Component {
         super(props);
         this.state = { forecasts: [], loading: true, weather: [], selectedLocation : null };
         this.displayOpinions = this.displayOpinions.bind(this);
-        
     }
 
     componentDidMount() {
